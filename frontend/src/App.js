1bcff77fd7c85969d278fabd4e3cd611b2608444
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2020-10-04 06:59:22 +0530

    Implemented React-Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,21 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from "./screens/ProductScreen";
+
 
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <Container>
-          <main><HomeScreen/></main>
+        <Route path='/' exact component={HomeScreen}/>
+        <Route path='/product/:id' component={ProductScreen}/>
       </Container>
       <Footer />
-    </>
+    </Router>
   );
 };
 
