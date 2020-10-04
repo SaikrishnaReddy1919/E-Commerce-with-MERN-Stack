058626e38c0367707c6d18aedbc4c441bdc0f458
SaikrishnaReddy1919 <krishnasvr9393@gmail.com>
2020-10-04 06:43:16 +0530

    Added sample images, header, footer, home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Krishna - Meena</h1>
-    </div>
+    <>
+      <Header />
+      <Container>
+          <main><HomeScreen/></main>
+      </Container>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
