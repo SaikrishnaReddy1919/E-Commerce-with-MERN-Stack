b436dc98d334ddae52e6607f119ac13fa8d1dc6d
Saikrishna Reddy <krishnasvr9393@gmail.com>
2020-10-05 16:52:36 +0530

    Added custom error handler

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+//IF requested for wrong url
+const notFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+//if there is any error in requesting the actual url
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.send.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export {notFound, errorHandler}
